d5a1e2cf21691d44766b962e3b3dc1c0062af686
David <david@Davids-MacBook-Air.local>
2026-04-17 12:24:51 +0200

    Made it all work

--- nano/main.py
+++ nano/main.py
@@ -1,13 +1,25 @@
-import raad_het_woord
+import raad_het_getal
 import galgje
-
-
-# welk spel wil je spelen?
-raad_het_woord.appLoop()
+import jokes
+import urllib3
 
 if __name__ == "__main__":
-    print("Welkom bij het galgje spel!")
-    print("Je zult een woord moeten raden door letters te raden.")
-    print("Je hebt 6 levens, elke keer dat je een fout maakt verlies je een leven.")
-    print("Raad het woord voordat je al je levens verliest!")
-    speel_sessie()
+    print("==== NANO Game Centre ====\n",
+          "Kies een getal en druk op enter om het spel te spelen:\n",
+          "1. Galgje\n",
+          "2. Raad het getal\n",
+          "3. Grappen generator\n",
+          "4. Stoppen\n")
+    
+    choice = input(">> ")
+
+    if choice == "1":
+        galgje.menu()
+    elif choice == "2":
+        raad_het_getal.app()
+    elif choice == "3":
+        jokes.app()
+    else:
+        print("GAME OVER")
+        exit()
+    
