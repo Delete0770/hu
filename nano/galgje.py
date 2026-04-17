addd63603f619780f550b247da2fb81fb3e5f0f8
David <david@Davids-MacBook-Air.local>
2026-04-17 12:23:53 +0200

    Deleted out quoted code

--- nano/galgje.py
+++ nano/galgje.py
@@ -4,12 +4,30 @@ import dutch_words
 import woorden
 import score
 
+cwd = os.getcwd()
+file_var = 'woordenCopy.txt'
+path = cwd + '/' + file_var
+
 wordDict = {}
-wordDict = woorden.lezen('/Users/david/Documents/github/hu/nano/woorden.txt')
+wordDict = woorden.lezen(path)
+
 for word in wordDict:
     wordDict[word] = len(word)
 
 wordDict = dict(sorted(wordDict.items(), key=lambda item: item[1]))
+
+def save_score(name, word, score):
+        # slaat naam, woord en score op in een bestand scores.txt
+    try:
+        with open(cwd + '/scores.txt', 'a') as file:
+            file.write(f"{name} | {word} | {score}\n")
+    except FileNotFoundError:
+        print('file not found\n')
+        print('create new file\n')
+
+        '''with open(cwd + '/scores.txt', 'x') as file:
+            file.write(f"name | word | score\n{name} | {word} | {score}\n")'''
+
 
 def toon_tussenstand(woord, geraden_letters):
     """
@@ -21,7 +39,7 @@ def toon_tussenstand(woord, geraden_letters):
     """
     for l in woord:
         if l in geraden_letters:
-            print(l, end="")
+            print(l.upper(), end="")
         else:
             print("_", end="")
 
@@ -85,26 +103,29 @@ def speel_sessie():
                 print(f"Fout. Je hebt nog {lifes} over.")
                 fout.append(guess)
                 print()
-            previous.append(guess)  
-        print(f"Foute letters: {fout}")     
+            previous.append(guess)
+        print(f"Foute letters: {fout}")
+        tries += 1
     score = lifes * int(hardness)
     print(f"woord: {word}, Score: {score}")
+    name = input("Save score. Voer je naam in: ")
+    save_score(name, word, score)
     
 
     if done == len(word):
         print("Gefeliciteerd, je hebt het woord geraden!")
     if lifes == 0:
         print("Helaas, je hebt geen levens meer over.")
+    
     print("GAME OVER")
 
 
 def menu():
     while True:
-        #lees_woorden('woorden.txt')
         print("=== Galgje Controller ===\n",
             "Kies een optie: \n",
             "1. Speel galgje\n",
-            "2. Wijzig een woord in de woordenlijst\n",
+            "2. Verwijder een woord uit de woordenlijst\n",
             "3. Voeg woord toe aan de woordenlijst\n",
             "4. Toon aantal woorden in de woordenlijst\n",
             "5. Stoppen\n")
@@ -114,15 +135,11 @@ def menu():
         if var == "1":
             speel_sessie()
         if var == "2":
-            #changeWord()
-            pass
+            word_del = input("Kies een woord om te verwijderen: ")
+            woorden.verwijderen(file_var, word_del)
         if var == "3":
-            var2 = input("Kies een wooord om toe te voegen: ")
-            print(f"variabele var2 bevat > {var2} en is het type {type(var2)}\n")
-            print(f"de functie woorden.graad(var2) returned > {woorden.graad(var2)} en is het type {type(woorden.graad(var2))}\n")
-            wordDict[var2] = float(woorden.graad(var2))
-            print(f"woord {var2} toegevoegd met moeilijkheidsgraad {wordDict[var2]}")
-            #sla_woorden_op('woorden.txt', var2)
+            word_add = input("Kies een wooord om toe te voegen: ")
+            woorden.toevoegen(file_var, word_add)
         if var == "4":
             lenWL = len(wordDict)
             print(f"Aantal woorden in woordenbestand: {lenWL}\n")
@@ -133,26 +150,3 @@ def menu():
 
 if __name__ == "__main__": 
     menu()
-
-'''
-print(f"WordList: {wordDict}\n")
-
-woorden.opslaan('woorden2.txt', wordDict)
-woorden.toevoegen('David', "woord", '1000')
-speel_sessie()
-
-
-
-
-dutch_words.bereken_score(aantal_levens_over, moeilijkheid)
-dutch_words.voeg_score_toe()
-
-woorden.kiezen(woorden_dict, moeilijkheidsgraad)
-woorden.lezen(bestandsnaam)
-woorden.lijst()
-woorden.opslaan(bestandsnaam, woorden_lijst)
-
-score.bereken(aantal_levens_over, moeilijkheid)
-score.toevoegen(naam, woord, score)
-score.toon_tussenstand(woord, geraden_letters)
-'''
